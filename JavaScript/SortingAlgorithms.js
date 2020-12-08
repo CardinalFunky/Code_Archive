91a6b5ea9af031c0ff61ae3efff485d367caaac1
Cardinal Funky <cardinalfunky@gmail.com>
2020-12-08 00:14:03 -0500

    stuff
    stuff

--- JavaScript/SortingAlgorithms.js
+++ JavaScript/SortingAlgorithms.js
@@ -1,19 +1,25 @@
 // Sorting Algorithms
 
 // Bubble Sort
-function bubbleSort(list) {
-    var complete = false;
-    var dummyList = list;
-    var flag = 0;
-    
+function bubbleSort_LeastToGreatest(list) {
+    let complete = false;
+    let dummyList = list;
+    let flag = 0;
+    let count = 0;
+
     while(!complete) {
-        for(var i = 0; i < dummyList.length; i++) {
+        count++;
+        console.log("Cycle #" + count);
+
+        for(let i = 0; i < dummyList.length; i++) {
             if(dummyList[i] > dummyList[i + 1]) {
                 flag += 1;
-                var temp = dummyList[i];
+                let temp = dummyList[i];
                 dummyList[i] = dummyList[i + 1];
                 dummyList[i + 1] = temp;
             }
+
+            console.log(dummyList);
         }
         
         if(flag == 0) {
@@ -27,5 +33,5 @@ function bubbleSort(list) {
 }
 
 // Algorithm Testing
-var testArray = [5, 4, 3, 2, 1, 0];
-print(bubbleSort(testArray));
+let testArray = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0];
+console.log(bubbleSort_LeastToGreatest(testArray));
